81b5759e98f116f62994b489bfe8ee6ecad9102b
Upal Patel <patelupal98@gmail.com>
2021-08-21 16:27:04 -0700

    implemented class-based components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Users from './components/Users';
+import UserFinder from "./components/UserFinder";
+import UsersContext from "./store/users-context";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Vandan" },
+  { id: "u2", name: "Parth" },
+  { id: "u3", name: "Yesha" },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS,
+  };
+
   return (
-    <div>
-      <Users />
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
 }
 
